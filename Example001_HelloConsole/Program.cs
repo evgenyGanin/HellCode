bb7abb8ac2fb6b4bed8ffc0b70180e12a117e103
evgeny_ganin <ufolklore@yandex.ru>
2023-01-11 20:49:14 +0300

    add Example 001 Hello console project

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// - два слэша это комментарий в коде
+//Console - инструмент работы с терминалом в качестве консоли
+//WriteLine - Вывод в окно терминала
+Console.WriteLine("Test raboti programmi");
+Console.WriteLine("Test raboti programmi");
+Console.WriteLine("Test raboti programmi");
